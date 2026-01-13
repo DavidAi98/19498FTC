23b19ccd700bf0a882939827de7461a408f4267d
lptoggled <jerrycywong@gmail.com>
2026-01-13 16:34:42 -0500

    adasdasdawdw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ServoTuner.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+@TeleOp
+@Config
+public class ServoTuner extends OpMode {
+    private Servo spindexer1, spindexer2;
+    public static double spindexer1Position, spindexer2Position;
+    public FtcDashboard dashboard = FtcDashboard.getInstance();
+    public TelemetryPacket packet = new TelemetryPacket();
+
+    public void init(){
+        spindexer1 = hardwareMap.get(Servo.class, "spindexer1");
+        spindexer2 = hardwareMap.get(Servo.class, "spindexer2");
+    }
+    public void loop(){
+        packet.put("Spindexer 1 Position", spindexer1Position);
+        packet.put("Spindexer 2 Position", spindexer2Position);
+        dashboard.sendTelemetryPacket(packet);
+        spindexer1.setPosition(spindexer1Position);
+        // 0.06 -> 3
+
+        spindexer2.setPosition(spindexer2Position);
+
+    }
+
+}
