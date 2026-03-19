c0cdb5c19183d974f51f9e8cd0349c10818bdd33
LPtoggled <jerrycywong@gmail.com>
2026-03-19 18:44:10 -0400

    Consistency Untested, Lots of changes in Constants.java, optimized some path, gate intake still somewhat sus. Try to optimize more by shooting earlier after gate intakes, making second row intake path smoother

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -36,7 +36,9 @@ public class Constants {
             .headingPIDFSwitch(10)
             .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(2,0,0.15,0.015))
 
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.04, 0, 0.0028, 0.4, 0.045))//line test contains the t and h pidf while drive tuner not; keep the same f with forwardtunner
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.05, 0, 0.003, 0.4, 0))//line test contains the t and h pidf while drive tuner not; keep the same f with forwardtunner
+            .drivePIDFSwitch(28)
+            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.03, 0, 0.006 , 0.4, 0))
 
             .centripetalScaling(0.0005);
 
